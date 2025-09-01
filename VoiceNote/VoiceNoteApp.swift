419676c076bc7ddb8f233733d0594d304ee65256
khyatimirani <khyatimirani1995@gmail.com>
2025-09-01 17:46:04 +0530

    Initial Commit

---
+++ VoiceNote/VoiceNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceNoteApp.swift
+//  VoiceNote
+//
+//  Created by Khyati Mirani on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceNoteTests/VoiceNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceNoteTests.swift
+//  VoiceNoteTests
+//
+//  Created by Khyati Mirani on 01/09/25.
+//
+
+import Testing
+@testable import VoiceNote
+
+struct VoiceNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
